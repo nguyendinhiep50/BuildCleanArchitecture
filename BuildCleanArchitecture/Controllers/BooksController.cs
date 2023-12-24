40c5dd5aeb0e1517c4c1ce24a490fd30af62a1d8
hiep <nguyendinhhiep317@gmail.com>
2023-12-24 22:26:05 +0700

    commit

---
+++ BuildCleanArchitecture/Controllers/BooksController.cs
@@ -0,0 +1,59 @@
+﻿using BuildCleanArchitecture.Application.Books.Commands;
+using BuildCleanArchitecture.Application.Books.Dtos;
+using BuildCleanArchitecture.Application.Books.Queries;
+using BuildCleanArchitecture.Application.Common.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuildCleanArchitecture.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ApiControllerBase
+    {
+        [HttpGet]
+        public Task<PaginatedList<BookDto>> Get([FromQuery] BookPagingFilterDto filter)
+        {
+            return Mediator.Send(new GetBooksQuery
+            {
+                Filter = filter
+            });
+        }
+
+        [HttpGet("{code}")]
+        public Task<BookDto> GetByCode([FromRoute] string code)
+        {
+            return Mediator.Send(new GetBookByBookCodeQuery
+            {
+                Id = code
+            });
+        }
+
+        [HttpPost]
+        public Task<ResponseModel<Boolean>> CreateUserAsync([FromBody] BookAddDto writeDto)
+        {
+            return Mediator.Send(new CreateBookCommand
+            {
+                Dto = writeDto
+            });
+        }
+
+        [HttpPut("{code}")]
+        public async Task<ResponseModel<bool>> UpdateUserAsync([FromRoute] string code, [FromBody] BookUpdateDto writeDto)
+        {
+            return await Mediator.Send(new UpdateBookCommand
+            {
+                Id = code,
+                Dto = writeDto
+            });
+        }
+
+        [HttpDelete("{code}")]
+        public Task<ResponseModel<bool>> DeleteUserAsync([FromRoute] string code)
+        {
+            return Mediator.Send(new DeleteBookCommand
+            {
+                Id = code
+            });
+        }
+    }
+}
---
+++ BuildCleanArchitecture/Program.cs
@@ -0,0 +1,35 @@
+using BuildCleanArchitecture;
+using BuildCleanArchitecture.Application;
+using BuildCleanArchitecture.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var services = builder.Services;
+var configuration = builder.Configuration;
+
+services.AddApplicationServices();
+services.AddInfrastructureServices(configuration);
+services.AddWebServices();
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
